1af72e560430a42dec7aeb11a807eb28d4b64bb8
Caio Lagreca <caiomiranda701@hotmail.com>
2022-11-07 11:44:19 -0300

    correção nome variavel

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,13 +59,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email }))._id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  const signautre = req.headers['stripe-signature'];
+  const signature = req.headers['stripe-signature'];
 
   let event;
   try {
